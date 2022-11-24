3ada5938d9df257b4365a1a66170e37ac0e84399
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2022-11-24 12:27:27 +0100

    add img

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const { createApp } = Vue;
+
+
+const app = createApp({
+    
+    data(){
+        return{
+           
+            };
+	    },
+		methods:{
+				
+			},
+		mounted:{
+			},
+				
+        
+}).mount('#app');
